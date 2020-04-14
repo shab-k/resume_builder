1b53d1c1ff810ace61b2cbb219be768787d216f4
shab-k <goldenblue.70732@gmail.com>
2020-04-14 12:37:31 -0400

    fixed the font size in the modular system.

--- dist/templates/footer.php
+++ dist/templates/footer.php
@@ -1,5 +1,5 @@
 	<footer>
-		<p><i class="far fa-copyright"></i> Copyright 2020 - </p>
-		<p>Made With <i class="fas fa-heart" style="color:#a5043a"></i> By Shabnam</p>
+		<p class="nano"><i class="far fa-copyright"></i> Copyright 2020 - </p>
+		<p class="nano">Made With <i class="fas fa-heart" style="color:#a5043a"></i> By Shabnam</p>
 	</footer>
 	</body>
